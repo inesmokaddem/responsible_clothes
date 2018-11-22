00f75fabd9f25155cc2c35eddb8557740d54508c
Diego Bolettieri <diego@bebetterhotels.com>
2018-11-22 16:45:44 +0100

    index running

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'scans#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products, only: [:show, :new, :create]
+  resources :products, only: [:index, :show, :new, :create]
   resources :scans, only: [:new, :create]
 end
