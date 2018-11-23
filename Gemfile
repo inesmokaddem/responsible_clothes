e10a67891cb295c0a03724ccc74ad535afbc69f0
inesmokaddem <ines.mokaddem@gmail.com>
2018-11-23 16:17:24 +0100

    downgrade cloudinary

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'uglifier'
 gem 'webpacker'
 gem 'inline_svg'
 
-gem 'cloudinary'
+gem 'cloudinary', '~> 1.9.1'
 gem 'carrierwave'
 
 group :development do
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def ecological_rating
+    water_footprint + carbon_footprint / 2
+  end
+
+  def water_footprint(footprint)
+    if footprint > 50000
+      1
+    elsif footprint > 40000
+      2
+    end
+  end
+
+  def global_rating(ecological_rating, social_rating)
+    ecological_rating + social_rating / 2
+  end
 end
